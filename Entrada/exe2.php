8ad54b825e0b37344d873289dc2c2e67703dfc69
DanielAlvesMorais <daltencedebora@mail.com>
2024-02-27 12:18:14 -0300

    Atividade

---
+++ Entrada/exe2.php
@@ -0,0 +1,8 @@
+<?php
+    $name= $_GET['name'];
+    $email= $_GET['email'];
+    $password= $_GET['password'];
+    echo "Seu nome é: ".$name."<br>";
+    echo "Seu email é: ".$email."<br>";
+    echo "Sua senha é: ".$password."<br>";
+?>
